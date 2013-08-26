9bc3f323f04127c1e436b2c2b2bcc64ea503caae
Evan Hemsley <evan.hemsley@gmail.com>
2013-08-25 21:51:29 -0700

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Thundercloud::Application.config.secret_token = '4487b9c0557f68a48fa588b2828d42afa2e004e7311bc86ebd0ec99099855bb107ebd8e82392c11276958b0754d5e234e3b71c177a6f9b0481124409d85b3cfa'
