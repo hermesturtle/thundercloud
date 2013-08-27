d17cad3391699ddc15fab4d8b91b82fbf18ee176
Evan Hemsley <evan.hemsley@gmail.com>
2013-08-27 10:05:01 -0700

    artist css + form

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -7,4 +7,22 @@ class ArtistsController < ApplicationController
     @artist = Artist.find(params[:id])
     @albums = @artist.albums
   end
+
+  def new
+    @artist = Artist.new
+  end
+
+  def create
+    @artist = Artist.create(params[:artist])
+    redirect_to artists_path
+  end
+
+  def edit
+    @artist = Artist.find(params[:id])
+  end
+
+  def update
+    artist = Artist.update_attributes(params[:artist])
+    redirect_to artist
+  end
 end
