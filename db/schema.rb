b29326b34e8bf8db3b1d9e3cf8f03a9caed4d17c
Evan Hemsley <evan.hemsley@gmail.com>
2013-08-27 11:12:30 -0700

    added image to artists

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130826055917) do
+ActiveRecord::Schema.define(:version => 20130827181143) do
 
   create_table "albums", :force => true do |t|
     t.string  "name"
@@ -22,6 +22,7 @@ ActiveRecord::Schema.define(:version => 20130826055917) do
   create_table "artists", :force => true do |t|
     t.string "name"
     t.string "description"
+    t.string "image"
   end
 
 end
