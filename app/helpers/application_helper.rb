4c73e04f53a3811944dd58680f47bddb6383095b
Evan Hemsley <evan.hemsley@gmail.com>
2013-09-03 14:19:14 -0700

    nav styling

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,4 +2,16 @@ module ApplicationHelper
   def price_in_dollars(price)
     number_to_currency(price / 100)
   end
+
+  def is_shop_page?(params)
+    params[:controller] == "products" || params[:controller] == "carts"
+  end
+
+  def is_artist_page?(params)
+    params[:controller] == "artists"
+  end
+
+  def is_about_page?(params)
+    params[:controller] == "static_pages" && params[:action] == "about"
+  end
 end
